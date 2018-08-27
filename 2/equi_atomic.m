ba1e1403b91271e3c18defbea3fc47a821155eed
VISHAL Subramanian <vishalsubbu97@gmail.com>
2018-08-27 11:23:02 +0530

    Update equi_atomic.m

--- 2/equi_atomic.m
+++ 2/equi_atomic.m
@@ -1,3 +1,13 @@
+%
+% Copyright (c) 2018, Vishal_S
+% All rights reserved. Please read the "license.txt" for license terms.
+%
+% Project Title: Computational Materials Thermodynamics
+% 
+% Developer: Vishal S
+% 
+% Contact Info: vishalsubbu97@gmail.com
+%
 clear all
 clc
 clf
